1048cae529a646579c8eb296a7574e79eb229ffb
Azizahfatmas <92080188+Azizahfatmas@users.noreply.github.com>
2022-02-25 11:25:40 +0700

    Add files via upload

---
+++ praktikum1/coba.php
@@ -0,0 +1,8 @@
+<?php 
+$nama = "Azizah";
+$jenis_kelamin = "Permpuan";
+$alamatSaya = "Depok"
+echo "Nama saya $nama";
+echo "Jenis kelamin saya $jenis_kelamin";
+echo "Alamat saya $alamatSaya";
+?>
---
+++ praktikum1/variabel-user.php
@@ -0,0 +1,12 @@
+<?php
+//Definisikan variabel
+$nama = "Azizah Fatma Sunardi";
+$umur = 13;
+$berat = 35.5;
+
+echo "Nama : $nama";
+echo "<br>Umur : $umur Tahun ";
+echo "<br>Berat : $berat Kg ";
+
+echo"<br> Hello $nama Apa Kabar";
+?>
